6dc942ea87e5f158ef0ba305721d9b1c6d7ce7c5
Lyell <lyelln@reed.edu>
2026-01-27 23:38:45 -0800

    Added prevention for floor clipping with jump

--- Assets/Scripts/Physics.cs
+++ Assets/Scripts/Physics.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 public class Physics : MonoBehaviour
 {
 
+    Collider2D[] results; //array that stores colliders the box comes into contact with
+    
+    //filter for filtering out types of colliders that player can come into contact with
+    //can use to, for example, filter obstacle vs wall colliders for different behaviors 
+    //(currently just ignored, as no colliders have a filter yet)
+    //ContactFilter2D filter; 
+
+
     public float distanceToCheck = 0.25f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        results = new Collider2D[1]; //initialize length to 1
     }
 
     // Update is called once per frame
@@ -18,12 +26,18 @@ public class Physics : MonoBehaviour
     }
     
     //returns T/F if object is grounded
-    public bool Grounded (Transform objTransform, Vector2 boxPoint, Vector2 boxSize, float boxAngle)
+
+    public Collider2D Grounded (Transform objTransform, Vector2 boxPoint, Vector2 boxSize, float boxAngle)
     {   
 
+        if (Physics2D.OverlapBox(boxPoint, boxSize, boxAngle, ContactFilter2D.noFilter, results) > 0)
+        {
+            return results[0];
+        }
+
+
+        return null;
         // Debug.DrawRay(objTransform.position, Vector3.down, Color.black);
         //Debug.Log(objTransform.position);
-
-        return Physics2D.OverlapBox(boxPoint, boxSize, boxAngle);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour
     public GameObject physicsSystem;
     private Physics physicsScript;
 
+    public float playerHeight; 
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,15 +26,18 @@ public class Player : MonoBehaviour
     {
 
         verticalVelocity += gravity * Time.deltaTime;
+        Collider2D groundCollider = physicsScript.Grounded(transform, transform.position, transform.localScale, 0);
         
-        if (physicsScript.Grounded(transform, transform.position, transform.localScale, 0) && verticalVelocity <= 0)
+        if (groundCollider && verticalVelocity <= 0)
         {
-            Debug.Log("setting velocity to 0");
+            // Debug.Log("setting velocity to 0");
+            Vector2 groundPos = Physics2D.ClosestPoint(transform.position, groundCollider);
+            transform.position = new Vector2(transform.position.x, groundPos.y + (playerHeight / 2));
             verticalVelocity = 0.0f;
 
         }
 
-        Debug.Log("updating velocity");
+        // Debug.Log("updating velocity");
         transform.Translate(new Vector2(0, verticalVelocity) * Time.deltaTime);
 
     }
