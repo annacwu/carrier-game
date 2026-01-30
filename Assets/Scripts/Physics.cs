a24c554d3e92196300955e1fc637d41125e8c277
Lyell <lyelln@reed.edu>
2026-01-29 22:11:44 -0800

    Added raycasting to predict collisions

--- Assets/Scripts/Physics.cs
+++ Assets/Scripts/Physics.cs
@@ -40,4 +40,52 @@ public class Physics : MonoBehaviour
         // Debug.DrawRay(objTransform.position, Vector3.down, Color.black);
         //Debug.Log(objTransform.position);
     }
+
+    //sends rays out from object based on velocity to see whether it will hit anything this frame
+    //currently gets all colliders, but should be modified to filter only ground once we implement that
+    //returns coordinate of impact point (i.e. where the player should snap to)
+    public Vector2 VerticalImpact (Transform objTransform, Vector2 objVelocity, int numRays)
+    {
+        //distance: pythagorean theorem
+        //float maxDist = Mathf.Sqrt((objVelocity.x * objVelocity.x) + (objVelocity.y * objVelocity.y));
+        float maxDist = objVelocity.magnitude;
+        //Debug.Log("max distance is " + maxDist);
+
+        //distance that closest collider is to the player
+        float minDist = Mathf.Infinity;
+        //Vector2 will return a zero vector if there is no result
+        Vector2 returnVector = Vector2.zero;
+
+        //get height and width beforehand for readability        
+        float height = objTransform.localScale.y;
+        float width = objTransform.localScale.x;
+        
+        for (int i = 0; i < numRays; i++)
+        {   
+            //Debug.Log("creating rays!");
+            float offset = -(width / 2.0f) + (width / numRays * i);
+            //where each ray should start - basically, start at left corner of box and then move right in increments based on numRays
+            Vector2 origin =  new Vector2(objTransform.position.x + offset, objTransform.position.y - (height / 2.0f));
+
+            //currently no layerMask bc we haven't implemented that, but seems to be an int - so colliders would have to be on different layers?
+            RaycastHit2D hit = Physics2D.Raycast(origin, objVelocity.normalized, maxDist);
+            
+            //if we hit something, if it is the closest thing so far, we should return it
+            if (hit)
+            {
+                if (minDist > hit.distance)
+                {
+                    minDist = hit.distance;
+                    //get point where that ray intersected with a collider
+                    Vector2 hitPoint = hit.point;
+                    float newX = hitPoint.x - offset;
+                    returnVector = new Vector2(newX, hitPoint.y);
+                }
+            }
+        }
+        
+        return returnVector;
+    }
+
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,16 @@ public class Player : MonoBehaviour
 {
 
     public float jumpForce;
+    public int numRays; 
     public float gravity = -9.81f;
-    private float verticalVelocity; 
+    public bool useForgivingJumps = false;
+    public int forgivenessFactor = 1; //determines how many frames before hitting the ground u can press jump
+
+    private bool jumpSaved = false;
+    private int tick = 0;
+
+    private float verticalVelocity; //stores vertical velocity
+    private float horizontalVelocity = 0; //stores horizontal velocity - currently set to 0 since we don't have that, BUT MAKE SURE TO USE THIS VARIABLE ONCE WE DO!!
     public GameObject physicsSystem;
     private Physics physicsScript;
 
@@ -24,16 +32,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //logic for forgetting saved jumps if they are not executed in time
+        if (jumpSaved && tick <= forgivenessFactor)
+        {
+            tick += 1;
+        } else if (jumpSaved)
+        {
+            tick = 0;
+            jumpSaved = false;
+        }
 
         verticalVelocity += gravity * Time.deltaTime;
-        Collider2D groundCollider = physicsScript.Grounded(transform, transform.position, transform.localScale, 0);
         
-        if (groundCollider && verticalVelocity <= 0)
+        //calculated distance object will move in next frame
+        Vector2 prelimTravel = new Vector2(horizontalVelocity * Time.deltaTime, verticalVelocity * Time.deltaTime);
+        Vector2 hitLoc = physicsScript.VerticalImpact(transform, prelimTravel, numRays); //gets point of potential impact
+
+        //Collider2D groundCollider = physicsScript.Grounded(transform, transform.position, transform.localScale, 0);
+        
+        if (hitLoc != Vector2.zero && verticalVelocity <= 0)
         {
             // Debug.Log("setting velocity to 0");
-            Vector2 groundPos = Physics2D.ClosestPoint(transform.position, groundCollider);
-            transform.position = new Vector2(transform.position.x, groundPos.y + (transform.localScale.y / 2));
+            //Vector2 groundPos = Physics2D.ClosestPoint(transform.position, groundCollider);
+            transform.position = new Vector2(hitLoc.x, hitLoc.y + (transform.localScale.y / 2));
             verticalVelocity = 0.0f;
+
+            //if there is a 'saved' jump, execute it
+            if (jumpSaved)
+            {
+                jumpSaved = false;
+                tick = 0;
+                verticalVelocity += jumpForce;
+            }
 
         }
 
@@ -42,11 +72,24 @@ public class Player : MonoBehaviour
 
     }
 
+    //onJump still uses Grounded since we want u to be currently on the floor and not just about to hit it
+    //but doesn't need to, especially if we want to do something like forgiving jumps
+    //(i added a toggle to test this)
     void OnJump ()
     {
-        if (physicsScript.Grounded(transform, transform.position, transform.localScale, 0))
+        //if player presses jump button and they are currently in the air, 'save' their input for a few frames (based on forgiveness factor)
+        //if they end up on the ground in any of those frames, "forgive" their jump (i.e. execute it)
+        if (useForgivingJumps)
         {
-            verticalVelocity += jumpForce;
+            jumpSaved = true;
+
+        } else
+        {
+            if (physicsScript.Grounded(transform, transform.position, transform.localScale, 0))
+            {
+                verticalVelocity += jumpForce;
+            }
         }
+
     }
 }
