c820d86f629a78fa6a7633acb008e369ce9d6981
lyelllionel <lyell@obviously.com>
2026-02-12 11:46:08 -0800

    Raycast (#22)
    * Added prevention for floor clipping with jump
    
    * Made height calculations for snapping more elegant
    
    * Added raycasting to predict collisions
    
    * Added raycasts for all directions
    
    * Addressed some comments and added ray offest
    
    * addressed some anna comments
    
    * extract repetitive impact code
    
    * cleanup code
    
    * add enum and extract player update
    
    ---------
    
    Co-authored-by: Lyell <lyelln@reed.edu>
    Co-authored-by: annacwu <annacwu04@gmail.com>

--- Assets/Scripts/Physics.cs
+++ Assets/Scripts/Physics.cs
@@ -1,8 +1,23 @@
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
+public enum CollisionDirection
+{
+    None, 
+    Horizontal, 
+    Vertical
+}
+
 public class Physics : MonoBehaviour
 {
+    // Array that stores colliders the box comes into contact with
+    Collider2D[] results;
+
+    // Offset between the corner of the collider and where the first ray is sent out from.
+    // Fixes some edge cases where Impact() wouldn't know whether the collision is vertical or horizontal.
+    // Makes effective 'collider' smaller, needs to be synced up with smaller Grounded() box (NOT CURRENTLY IMPLEMENTED).
+    public float rayOffset = 0.01f;
 
     Collider2D[] results; //array that stores colliders the box comes into contact with
     
@@ -13,31 +28,192 @@ public class Physics : MonoBehaviour
 
 
     public float distanceToCheck = 0.25f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        results = new Collider2D[1]; //initialize length to 1
+        results = new Collider2D[1];
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() { }
+
+    /// <summary>
+    /// Returns the collider that OverlapBox hits, or null if nothing is hit.
+    /// </summary>
+    public Collider2D Grounded(Vector2 boxPoint, Vector2 boxSize, float boxAngle)
     {
-        
-    }
-    
-    //returns T/F if object is grounded
-
-    public Collider2D Grounded (Transform objTransform, Vector2 boxPoint, Vector2 boxSize, float boxAngle)
-    {   
-
-        if (Physics2D.OverlapBox(boxPoint, boxSize, boxAngle, ContactFilter2D.noFilter, results) > 0)
+        if (
+            Physics2D.OverlapBox(boxPoint, boxSize, boxAngle, ContactFilter2D.noFilter, results) > 0
+        )
         {
             return results[0];
         }
 
-
         return null;
-        // Debug.DrawRay(objTransform.position, Vector3.down, Color.black);
-        //Debug.Log(objTransform.position);
+    }
+
+    /// <summary>
+    /// Sends rays out from an object based on velocity to predict collisions this frame.
+    /// Returns the coordinate of the impact point (i.e. where the player should snap to).
+    /// If an impact is detected, call this again to check for a second impact in the same frame
+    /// (e.g. hitting a corner where both horizontal and vertical velocity cause an impact).
+    /// Two checks should be enough for any scenario.
+    /// </summary>
+    /// <param name="objTransform">Transform of the object checking for collisions (e.g. the player).</param>
+    /// <param name="objVelocity">Velocity of the object.</param>
+    /// <param name="numRays">Number of rays sent from each side (e.g. 3 sends up to 3 from each edge).</param>
+    /// <param name="direction">Returns which axis a collision was detected on: "horizontal", "vertical", or "none".</param>
+    public Vector2 Impact(
+        Transform objTransform,
+        Vector2 objVelocity,
+        int numRays,
+        out CollisionDirection direction
+    )
+    {
+        // Distance that the closest collider is to the player
+        float minDist = Mathf.Infinity;
+        // Returns a zero vector if there is no result
+        Vector2 returnVector = Vector2.zero;
+
+        // Get height and width beforehand for readability (corrected for rayOffset)
+        float height = objTransform.localScale.y - rayOffset * 2;
+        float width = objTransform.localScale.x - rayOffset * 2;
+
+        direction = CollisionDirection.None;
+
+        for (int i = 0; i < numRays; i++)
+        {
+            // Check for horizontal collisions
+            var (horizontalHit, side, vertOffset) = CheckAxis(
+                0,
+                objVelocity,
+                objTransform,
+                i,
+                height,
+                width,
+                numRays
+            );
+
+            if (horizontalHit)
+            {
+                var (newReturn, newMin, updated) = CalculateHit(
+                    0,
+                    horizontalHit,
+                    side,
+                    vertOffset,
+                    minDist,
+                    height,
+                    width
+                );
+                if (updated)
+                {
+                    returnVector = newReturn;
+                    minDist = newMin;
+                    direction = CollisionDirection.Horizontal;
+                }
+            }
+
+            // Check for vertical collisions
+            var (verticalHit, vert, horizontalOffset) = CheckAxis(
+                1,
+                objVelocity,
+                objTransform,
+                i,
+                height,
+                width,
+                numRays
+            );
+
+            if (verticalHit)
+            {
+                var (newReturn, newMin, updated) = CalculateHit(
+                    1,
+                    verticalHit,
+                    vert,
+                    horizontalOffset,
+                    minDist,
+                    height,
+                    width
+                );
+                if (updated)
+                {
+                    returnVector = newReturn;
+                    minDist = newMin;
+                    direction = CollisionDirection.Vertical;
+                }
+            }
+        }
+
+        return returnVector;
+    }
+
+    /// <summary>
+    /// Casts a ray along the given axis and returns the hit info.
+    /// Axis: 0 for horizontal, 1 for vertical.
+    /// </summary>
+    private (RaycastHit2D hit, int dir, float offset) CheckAxis(
+        int axis,
+        Vector2 objVelocity,
+        Transform objTransform,
+        int i,
+        float height,
+        float width,
+        int numRays
+    )
+    {
+        if (objVelocity[axis] != 0)
+        {
+            float maxDist = objVelocity.magnitude;
+
+            // Get direction (up/down or left/right)
+            int dir = objVelocity[axis] < 0 ? -1 : 1;
+
+            // Size of the player on the axis in the direction we are moving
+            float movementSize = axis == 0 ? width : height;
+
+            // Size of the player on the axis the rays will be spread across
+            float spreadSize = axis == 0 ? height : width;
+
+            float offset = -(spreadSize / 2.0f) + (spreadSize / (numRays - 1) * i);
+
+            // Calculate origin
+            Vector2 origin = Vector2.zero;
+            origin[axis] = objTransform.position[axis] + (movementSize / 2.0f * dir);
+            origin[1 - axis] = objTransform.position[1 - axis] + offset;
+
+            RaycastHit2D hit = Physics2D.Raycast(origin, objVelocity, maxDist);
+            return (hit, dir, offset);
+        }
+
+        return (default, 0, 0);
+    }
+
+    /// <summary>
+    /// Calculates the snap position from a raycast hit if it's closer than the current minimum.
+    /// Axis: 0 for horizontal, 1 for vertical.
+    /// </summary>
+    private (Vector2 collision, float minDist, bool updated) CalculateHit(
+        int axis,
+        RaycastHit2D hit,
+        int dir,
+        float offset,
+        float currentMin,
+        float height,
+        float width
+    )
+    {
+        if (currentMin > hit.distance)
+        {
+            // Get point where that ray intersected with a collider
+            Vector2 hitPoint = hit.point;
+            float otherCoord = hitPoint[1 - axis] - offset;
+            Vector2 returnVector =
+                axis == 0
+                    ? new Vector2(hitPoint[axis] - (width / 2 * dir), otherCoord)
+                    : new Vector2(otherCoord, hitPoint[axis] - (height / 2 * dir));
+
+            return (returnVector, hit.distance, true);
+        }
+
+        return (default, currentMin, false);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,16 @@ public class Player : MonoBehaviour
 {
 
     public float jumpForce;
+    public int numRays; 
     public float gravity = -9.81f;
-    private float verticalVelocity; 
+    public bool useForgivingJumps = false;
+    public int forgivenessFactor = 1; //determines how many frames before hitting the ground u can press jump
+
+    private bool jumpSaved = false;
+    private int tick = 0;
+
+    private float verticalVelocity; //stores vertical velocity
+    private float horizontalVelocity = 0; //stores horizontal velocity - currently set to 0 since we don't have that, BUT MAKE SURE TO USE THIS VARIABLE ONCE WE DO!!
     public GameObject physicsSystem;
     private Physics physicsScript;
 
@@ -21,32 +29,109 @@ public class Player : MonoBehaviour
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        verticalVelocity += gravity * Time.deltaTime;
-        Collider2D groundCollider = physicsScript.Grounded(transform, transform.position, transform.localScale, 0);
-        
-        if (groundCollider && verticalVelocity <= 0)
-        {
-            // Debug.Log("setting velocity to 0");
-            Vector2 groundPos = Physics2D.ClosestPoint(transform.position, groundCollider);
-            transform.position = new Vector2(transform.position.x, groundPos.y + (transform.localScale.y / 2));
-            verticalVelocity = 0.0f;
-
-        }
-
-        // Debug.Log("updating velocity");
-        transform.Translate(new Vector2(0, verticalVelocity) * Time.deltaTime);
-
+        HandleForgivingJumps();
+        ApplyGravityAndMove();
     }
 
-    void OnJump ()
+    void HandleForgivingJumps()
     {
-        if (physicsScript.Grounded(transform, transform.position, transform.localScale, 0))
+        //logic for forgetting saved jumps if they are not executed in time
+        if (jumpSaved && tick <= forgivenessFactor)
         {
+            tick += 1;
+        } else if (jumpSaved)
+        {
+            tick = 0;
+            jumpSaved = false;
+        }
+
+        //if there is a 'saved' jump, execute it
+        Collider2D groundCollider = physicsScript.Grounded(transform.position, transform.localScale, 0);
+        if (jumpSaved && groundCollider)
+        {
+            jumpSaved = false;
+            tick = 0;
             verticalVelocity += jumpForce;
         }
+    }
+
+    void ApplyGravityAndMove()
+    {
+        verticalVelocity += gravity * Time.deltaTime;
+
+        //calculated distance object will move in next frame
+        Vector2 prelimTravel = new Vector2(horizontalVelocity * Time.deltaTime, verticalVelocity * Time.deltaTime);
+        CollisionDirection impact;
+        Vector2 hitLoc = physicsScript.Impact(transform, prelimTravel, numRays, out impact); // gets point of potential impact
+
+        if (impact != CollisionDirection.None)
+        {
+            ResolveCollision(prelimTravel, hitLoc, impact);
+        } else
+        {
+            transform.Translate(new Vector2(horizontalVelocity, verticalVelocity) * Time.deltaTime); //if no impact, move as normal
+        }
+    }
+
+    void ResolveCollision(Vector2 prelimTravel, Vector2 hitLoc, CollisionDirection impact)
+    {
+        Vector2 oldPos = transform.position;
+        Vector2 snapTravel = hitLoc - oldPos; // distance that (if we hit something) was travelled
+
+        transform.position = hitLoc; //make snap
+
+        float remainingVertical = prelimTravel.y - snapTravel.y;
+        float remainingHorizontal = prelimTravel.y - snapTravel.y;
+        Vector2 remainingTravel = new Vector2(remainingHorizontal, remainingVertical); //calculate remaining distance you would have moved if not for the snap
+
+        Vector2 secondHitLoc;
+        CollisionDirection secondImpact;
+
+        if (impact == CollisionDirection.Horizontal)
+        {
+            horizontalVelocity = 0.0f;
+            remainingTravel.x = 0; //if we made a horizontal impact, we should not travel any more on that axis
+            secondHitLoc = physicsScript.Impact(transform, new Vector2(0, remainingVertical), numRays, out secondImpact);
+        } else
+        {
+            verticalVelocity = 0.0f;
+            remainingTravel.y = 0;
+            secondHitLoc = physicsScript.Impact(transform, new Vector2(remainingHorizontal, 0), numRays, out secondImpact);
+        }
+
+        //if there was a second impact, we can safely set all velocity to 0, since we know we hit colliders in both directions of travel
+        if (secondImpact != CollisionDirection.None)
+        {
+            transform.position = secondHitLoc; //add new snap travel
+            verticalVelocity = 0;
+            horizontalVelocity = 0;
+            remainingTravel = Vector2.zero; //set all velocity things to 0
+        }
+
+        //move only in direction where we didn't initially make impact
+        transform.Translate(remainingTravel);
+    }
+
+    //onJump still uses Grounded since we want u to be currently on the floor and not just about to hit it
+    //but doesn't need to, especially if we want to do something like forgiving jumps
+    //(i added a toggle to test this)
+    void OnJump ()
+    {
+        //if player presses jump button and they are currently in the air, 'save' their input for a few frames (based on forgiveness factor)
+        //if they end up on the ground in any of those frames, "forgive" their jump (i.e. execute it)
+        if (useForgivingJumps)
+        {
+            jumpSaved = true;
+
+        } else
+        {
+            if (physicsScript.Grounded(transform.position, transform.localScale, 0))
+            {
+                verticalVelocity += jumpForce;
+            }
+        }
+
     }
 }
